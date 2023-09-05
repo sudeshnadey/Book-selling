a682280cbe84f1141eb10d9414272c02df720370
Hundaol NK <hundaolnk@gmail.com>
2023-09-05 16:51:33 +0300

    worked on product page

--- Product/product.js
+++ Product/product.js
@@ -1,66 +1,78 @@
+const token = localStorage.getItem("user");
 function fetchDataFromAPI() {
-  fetch('http://api.bhattacharjeesolution.in/book/api/product/admin-show.php', {
-    headers: {
-      'Content-Type': 'application/json'
-  },
-  }) // Replace with your API endpoint
+  fetch(
+    "https://api.bhattacharjeesolution.in/book/api/admin-show-product.php",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        token: token,
+      },
+    }
+  ) // Replace with your API endpoint
     .then(function (response) {
       console.log(response);
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       return response.json(); // Parse the response as JSON
     })
     .then(function (data) {
       // Call the populateBannerTableWithData function with the retrieved data
       populateBannerTable(data);
-        console.log('data:', data);
+      console.log("data:", data);
     })
     .catch(function (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     });
-  }
+}
 
-  // Function to populate the banner table
-  function populateBannerTable(data) {
-    var bannerTable = document.getElementById("banner-table");
-    var bannerTableBody = document.getElementById("banner-table-body");
+// Function to populate the banner table
+function populateBannerTable(data) {
+  var bannerTable = document.getElementById("banner-table");
+  var bannerTableBody = document.getElementById("banner-table-body");
 
-    
   // Clear the existing rows in bannerTableBody
   while (bannerTableBody.firstChild) {
     bannerTableBody.removeChild(bannerTableBody.firstChild);
   }
-      data.forEach(function (item) {
-          var row = document.createElement("tr");
-  
-          row.innerHTML = `
+  data?.forEach(function (item) {
+    var row = document.createElement("tr");
+
+    row.innerHTML = `
               <td>${item.id}</td>
               <td>${item.name}</td>
-              <td>${item.description}</td>
               <td>
-              <img src="https://api.bhattacharjeesolution.in/book/api/images/${item.image.replace(' ', '%20')}" alt="" style="width: calc(80% - 30px); flex: 5; object-fit: cover; max-width: 100px; max-height: 100px;" />
+              <img src="${item.image.replace(
+                " ",
+                "%20"
+              )}" alt="" style="width: calc(80% - 30px); flex: 5; object-fit: cover; max-width: 100px; max-height: 100px;" />
               </td>
+              <td>${item.description}</td>
+              <td>${item.mrp}</td>
+              <td>${item.discount}</td>
+              <td>${item.quantity}</td>
               <td>
                   <button class="edit-button" style="border: none"  data-coupon='${JSON.stringify(
                     item
                   )}'>
                       <i class="fa-solid fa-pen"></i>
                     </button>
-                    <button style="border: none; color: red" onclick="deleteBanner(${item.id})">
+                    <button style="border: none; color: red" onclick="deleteBanner(${
+                      item.id
+                    })">
                       <i class="fa-solid fa-trash"></i>
                     </button>
               </td>
           `;
-  
-          bannerTableBody.appendChild(row);
-          var editButton = row.querySelector(".edit-button");
-          editButton.addEventListener("click", function () {
-            var couponData = JSON.parse(editButton.getAttribute("data-coupon"));
-            editBanner(couponData);
-          });
-      });
-  }
+
+    bannerTableBody.appendChild(row);
+    var editButton = row.querySelector(".edit-button");
+    editButton.addEventListener("click", function () {
+      var couponData = JSON.parse(editButton.getAttribute("data-coupon"));
+      editBanner(couponData);
+    });
+  });
+}
 // banner.js
 document.addEventListener("DOMContentLoaded", function () {
   var bannerTable = document.getElementById("banner-table");
@@ -68,73 +80,68 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Sample banner data (simulated delay for demonstration)
   setTimeout(function () {
+    // Call the populateBannerTable function with your banner data on initial render
+    // populateBannerTable(bannerData);
+    // Function to fetch data from the API
 
+    fetchDataFromAPI();
 
-      
-      // Call the populateBannerTable function with your banner data on initial render
-      // populateBannerTable(bannerData);
-      // Function to fetch data from the API
-      
-        fetchDataFromAPI();
+    // Add an event listener to the search input field
+    var searchInput = document.getElementById("searchbar");
+    searchInput.addEventListener("input", function () {
+      var searchText = searchInput.value.trim().toLowerCase();
 
-      // Add an event listener to the search input field
-      var searchInput = document.getElementById("searchbar");
-      searchInput.addEventListener("input", function () {
-          var searchText = searchInput.value.trim().toLowerCase();
+      // Loop through the table rows (skip the first row which is the header)
+      var rows = bannerTableBody.querySelectorAll("tr");
 
-          // Loop through the table rows (skip the first row which is the header)
-          var rows = bannerTableBody.querySelectorAll("tr");
+      for (var i = 0; i < rows.length; i++) {
+        var row = rows[i];
+        var nameColumn = row.querySelector("td:nth-child(2)"); // Target the "Banner Name" column (2nd column)
+        var nameText = nameColumn.textContent.trim().toLowerCase();
 
-          for (var i = 0; i < rows.length; i++) {
-              var row = rows[i];
-              var nameColumn = row.querySelector("td:nth-child(2)"); // Target the "Banner Name" column (2nd column)
-              var nameText = nameColumn.textContent.trim().toLowerCase();
-
-              if (nameText.includes(searchText)) {
-                  row.style.display = ""; // Show the row
-              } else {
-                  row.style.display = "none"; // Hide the row
-              }
-          }
-      });
-    }, 0); // No simulated delay in this example (adjust as needed)
-  });
-  
-  // Function to handle banner deletion
-  function deleteBanner(bannerNumber) {
-      // Add your code here to handle the deletion of the banner with the specified Number
-      // For example, you can show a confirmation dialog and then remove the banner from the table and data array
-      // alert("Banner with Number " + bannerNumber + " will be deleted.");
-      if (window.confirm("Are you sure?") == true) {
-        console.log(bannerNumber);
-        const formdata = new FormData();
-        formdata.append("bannerId", bannerNumber);
-        var requestData = {
-          bannerId: bannerNumber,
-      };
-        fetch(`https://api.bhattacharjeesolution.in/book/api/delete-banner.php`, {
-          method: 'POST',
-          // body: JSON.stringify(requestData),
-          body: formdata,
-          headers: {
-            'Content-Type': 'application/json'
-          },
-      }).then(function (response) {
-          console.log(response);
-          return response.json(); // Parse the response as JSON
-        })
-        .then(function (data) {
-          // Call the populateBannerTableWithData function with the retrieved data
-          // populateBannerTable(data);
-            console.log('data:', data);
-        })
-        .catch(function (error) {
-          console.error('Error fetching data:', error);
-        });
+        if (nameText.includes(searchText)) {
+          row.style.display = ""; // Show the row
+        } else {
+          row.style.display = "none"; // Hide the row
+        }
       }
-  }
+    });
+  }, 0); // No simulated delay in this example (adjust as needed)
+});
 
- // Flag to check if the submit event listener is already added
+// Function to handle banner deletion
+function deleteBanner(bannerNumber) {
+  // Add your code here to handle the deletion of the banner with the specified Number
+  // For example, you can show a confirmation dialog and then remove the banner from the table and data array
+  // alert("Banner with Number " + bannerNumber + " will be deleted.");
+  if (window.confirm("Are you sure?") == true) {
+    console.log(bannerNumber);
+    const formdata = new FormData();
+    formdata.append("id", bannerNumber);
+    fetch(`https://api.bhattacharjeesolution.in/book/api/delete-product.php`, {
+      method: "POST",
+      // body: JSON.stringify(requestData),
+      body: formdata,
+      headers: {
+        token: token,
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json(); // Parse the response as JSON
+      })
+      .then(function (data) {
+        // Call the populateBannerTableWithData function with the retrieved data
+        fetchDataFromAPI();
+        console.log("data:", data);
+      })
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+      });
+  }
+}
+
+// Flag to check if the submit event listener is already added
 let isSubmitEventListenerAdded = false;
 
 // Function to handle editing a banner
@@ -144,6 +151,9 @@ function editBanner(bannerData) {
   const editButtons = document.querySelectorAll(".edit-button");
   const closePopupButton = document.getElementById("closePopup");
   const bannerNameInput = document.getElementById("name");
+  const bannerQuantityInput = document.getElementById("quantity");
+  const bannerDiscountInput = document.getElementById("discount");
+  const bannerMrpInput = document.getElementById("mrp");
   const bannerImageInput = document.getElementById("fileInput");
   const bannerImagePreInput = document.getElementById("preimage");
   const bannerDescripitonInput = document.getElementById("description");
@@ -151,7 +161,10 @@ function editBanner(bannerData) {
   // Set the initial values in the form fields
   bannerNameInput.value = bannerData.name;
   bannerDescripitonInput.value = bannerData.description;
-  bannerImagePreInput.src = `https://api.bhattacharjeesolution.in/book/api/images/${bannerData.image.replace(' ', '%20')}` 
+  bannerImagePreInput.src = `${bannerData.image.replace(" ", "%20")}`;
+  bannerQuantityInput.value = bannerData.quantity;
+  bannerDiscountInput.value = bannerData.discount;
+  bannerMrpInput.value = bannerData.mrp;
 
   // Show the popup
   editButtons.forEach((button) => {
@@ -166,19 +179,32 @@ function editBanner(bannerData) {
     editForm.addEventListener("submit", async function (e) {
       e.preventDefault(); // Prevent the form from submitting normally (you can handle the submission logic here)
       // You can access the form data using bannerNameInput.value and bannerImageInput.files[0]
-      console.log("Form submitted:", bannerNameInput.value, bannerImageInput.files[0]);
+      console.log(
+        "Form submitted:",
+        bannerNameInput.value,
+        bannerImageInput.files[0]
+      );
 
       const formData = new FormData();
       formData.append("name", bannerNameInput.value);
+      formData.append("discount", bannerDiscountInput.value);
+      formData.append("quantity", bannerQuantityInput.value);
+      formData.append("mrp", bannerMrpInput.value);
       formData.append("description", bannerDescripitonInput.value);
       formData.append("image", bannerImageInput.files[0]);
-      formData.append("bannerId", bannerData.id);
+      formData.append("id", bannerData.id);
 
       try {
-        const response = await fetch("https://api.bhattacharjeesolution.in/book/api/edit-banner.php", {
-          method: "POST",
-          body: formData,
-        });
+        const response = await fetch(
+          "https://api.bhattacharjeesolution.in/book/api/edit-product.php",
+          {
+            method: "POST",
+            body: formData,
+            headers: {
+              token: token,
+            },
+          }
+        );
 
         if (response.ok) {
           // Handle success (e.g., display a success message)
@@ -202,4 +228,3 @@ function editBanner(bannerData) {
     editPopup.style.display = "none";
   });
 }
-
